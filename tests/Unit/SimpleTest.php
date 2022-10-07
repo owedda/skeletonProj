77f879e61f445aaf873e9ca8d8c04b8e9949bfe2
OvidijusRapalis <orapalis@gmail.com>
2022-10-07 13:38:14 +0300

    First commit

---
+++ tests/Unit/SimpleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ovidijus\PirmaUzd\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testTransformerBuildCollectionProperly(): void
+    {
+        $this->assertEquals(1, 1, "one equals to one");
+    }
+
+}
